e7f89730e39fe741b7eb415221b70eabce0289b3
malobs <malobamercy958@gmail.com>
2024-11-20 00:04:46 +0300

    creating the stars

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating  maxRating={10}/> 
-    <StarRating />
+    <StarRating  maxRating={5}/> 
+    
     
   </React.StrictMode>
 );
